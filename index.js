954efc08c0ec38940d0ba5c435cee81f6619f86e
6631503004KanyanatSawatdiwong <sugus.kanyanat@gmail.com>
2025-06-25 01:54:54 +0700

    index and others

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+}); //route handler
+
+const PORT = process.env.PORT || 5001; //if there isn't env vari defined by host, go assign vari to port 5001 (default)
+app.listen(PORT); //Express tells node to listen to traffic coming on port 5001
